df7af9a8327940e38d7910ff3184d2a3dd86f2ec
Cory Billington <mcorybillington@gmail.com>
2018-01-28 21:56:11 -0500

    Basic gui built. Works when called from main.py.

--- main.py
+++ main.py
@@ -5,36 +5,47 @@
 from cryptoFunctions import SendCipherText
 from diffhellman import MasterSecret
 import sys
+from PyQt5 import uic, QtWidgets, QtGui, QtCore
 # import time  # for testing
+
+class AppWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(AppWindow, self).__init__()
+        uic.loadUi('cyfr2_gui.ui', self)
+        self.show()
+
+app = QtWidgets.QApplication(sys.argv)
+w = AppWindow()
+sys.exit(app.exec_())
 
 # start_time = time.time()
 # lock = b'NDc5Mzg1MjYzYTlmOTNlOGZiZjZkNDFhYWM0MDljYzE='
 # unlock = b'NDc5Mzg1MjYzYTlmOTNlOGZiZjZkNDFhYWM0MDljYzE='
 # print("Time to generate keys: %s seconds" % (time.time() - start_time))
 
-lock, unlock = MasterSecret.private_key()
-
-
-def control():
-        char = input("Press y to send a message: ")
-        char = char.lower()
-        if char != 'y':
-            sys.exit(0)
-        else:
-            message = input('Enter text: ')
-            SendCipherText.send(message, lock)
-            control()
-
-
-def main():
-        print("Encrypted messaging app")
-        print('')
-        control()
-
-
-if __name__ == "__main__":
-    main()
-
+# lock, unlock = MasterSecret.private_key()
+#
+#
+# def control():
+#         char = input("Press y to send a message: ")
+#         char = char.lower()
+#         if char != 'y':
+#             sys.exit(0)
+#         else:
+#             message = input('Enter text: ')
+#             SendCipherText.send(message, lock)
+#             control()
+#
+#
+# def main():
+#         print("Encrypted messaging app")
+#         print('')
+#         control()
+#
+#
+# if __name__ == "__main__":
+#     main()
+#
 '''
 def keygen():
     key = Fernet.generate_key()
