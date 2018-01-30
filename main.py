8fb38ca2d04d2f89608235a1fdb95703e80790eb
Cory Billington <mcorybillington@gmail.com>
2018-01-29 21:33:27 -0500

    Basic GUI configured and working.

--- main.py
+++ main.py
@@ -4,15 +4,25 @@
 # from cryptography.fernet import Fernet
 from cryptoFunctions import SendCipherText
 from diffhellman import MasterSecret
-import sys
+import sys, threading
 from PyQt5 import uic, QtWidgets, QtGui, QtCore
 # import time  # for testing
+
 
 class AppWindow(QtWidgets.QDialog):
     def __init__(self):
         super(AppWindow, self).__init__()
         uic.loadUi('cyfr2_gui.ui', self)
         self.show()
+        self.sendButton.clicked.connect(self.send_msg)
+
+    def send_msg(self):
+        message = str(self.msgInputTextBox.toPlainText())
+        self.textBrowser.append("they:" + message)
+        self.msgInputTextBox.setText("")
+
+#    def generate_master_secret(self):
+
 
 app = QtWidgets.QApplication(sys.argv)
 w = AppWindow()
