440d1b9f68bb024f180badac019ca2f5c4039f69
mcorybillington <mcorybillington@gmail.com>
2018-07-28 20:50:56 -0400

    restructure of threading

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-#!/usr/bin/env python
-
 import sys
-import threading
 import socket
-from PyQt5 import uic, QtWidgets, QtGui, QtCore
-# import time  # for testing
+from PyQt5 import uic
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtCore import QThread, pyqtSignal
 
 
-class AppWindow(QtWidgets.QDialog):
+class AppWindow(QDialog):
     def __init__(self):
         super(AppWindow, self).__init__()
         uic.loadUi('cyfr2_gui.ui', self)
         self.msgInputLineEdit.setPlaceholderText("Send cyfr message")
         self.chat_listener = ChatListener()
+        self.chat_listener.status.connect(self.print_msg)
         self.chat_listener.start()
-        self.chat_sender = ChatSender()
         self.sendButton.setEnabled(False)
         self.msgInputLineEdit.textChanged.connect(self.check)
         self.sendButton.clicked.connect(self.send_msg)
         self.msgInputLineEdit.returnPressed.connect(self.click)
-        # self.connectButton.clicked.connect(self.connect)
         self.setFocus()
         self.show()
 
@@ -34,61 +31,58 @@ class AppWindow(QtWidgets.QDialog):
         if self.sendButton.isEnabled():
             self.sendButton.clicked.emit()
 
-    def receive(self):
-        chat_listener = ChatListener()
-        chat_listener.start()
-        message = chat_listener.run()
-        self.textBrowser.append("Them: " + message)
-
-    def key_exchange(self):
-        ip = self.ipLineEdit.text()
-        chat_sender = ChatSender(ip)
-        chat_sender.port = 80
-        chat_sender.address = ip
-        self.textBrowser.append("Exchanging Keys...")
-        chat_sender.start()
-        self.textBrowser.append("Key exchange complete!")
+    def print_msg(self, sender, val):
+        self.textBrowser.append(sender + " : " + val)
 
     def send_msg(self):
-        ip = self.ipLineEdit.text()
-        message = self.msgInputLineEdit.text()
-        chat_sender = ChatSender(ip, message)
+        chat_sender = ChatSender()
+        chat_sender.ip = self.ipLineEdit.text()
+        chat_sender.message = self.msgInputLineEdit.text()
         chat_sender.run()
-        self.textBrowser.append("Me: " + message)
+        while chat_sender.isRunning():
+            AppWindow.processevents()
+        chat_sender.terminate()
         self.msgInputLineEdit.setText('')
 
 
-class ChatListener(threading.Thread):
+class ChatListener(QThread):
+    status = pyqtSignal(str, str)
+
     def __init__(self):
-        threading.Thread.__init__(self)
+        QThread.__init__(self)
         self.ip = ''
         self.port = 5004
+        self.message = ''
+        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def run(self):
-        print("hi")
-        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listen_socket.bind((self.ip, self.port))
-        listen_socket.listen(1)
-        connection, address = listen_socket.accept()
-        message = connection.recv(2048)
+        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_socket.bind((self.ip, self.port))
+        self.listen_socket.listen(1)
+        while True:
+            connection, address = self.listen_socket.accept()
+            self.message = connection.recv(2048).decode('utf-8')
+            address = str(address).split(',')[0].strip('[]\'(')
+            self.status.emit(address, self.message)
 
 
-class ChatSender(threading.Thread):
+class ChatSender(QThread):
+    finished = pyqtSignal('PyQt_PyObject')
+
     def __init__(self):
-        threading.Thread.__init__(self)
+        QThread.__init__(self)
         self.address = ''
-        self.port = 5001
+        self.port = 5004
         self.message = ''
+        self.send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self):
-        send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        send_socket.connect((self.address, self.port))
-        send_socket.sendall(self.message)
+    def run(self):
+        self.send_socket.connect((self.address, self.port))
+        self.send_socket.sendall(self.message.encode('utf-8'))
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     w = AppWindow()
     sys.exit(app.exec_())
 
