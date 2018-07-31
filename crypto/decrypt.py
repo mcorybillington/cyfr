0aa8c2eae192f40b045ad54f111a7e6de8c2337f
mcorybillington <mcorybillington@gmail.com>
2018-07-30 22:13:38 -0400

    modified to work with new crypto key library

--- crypto/decrypt.py
+++ crypto/decrypt.py
@@ -1,17 +1,19 @@
+import base64
 from Crypto.Cipher import AES
 
 
 class DecryptAES(object):
     def __init__(self):
+        self.bs = 16
         self.enc = ''
         self.private_key = ''
 
     def decrypt(self):
-        iv = self.enc[:AES.block_size]
+        enc = base64.b64decode(self.enc)
+        iv = enc[:self.bs]
         cipher = AES.new(self.private_key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(self.enc[AES.block_size:])).decode()
+        return self._unpad(cipher.decrypt(enc[self.bs:])).decode('utf-8')
 
     @staticmethod
     def _unpad(s):
-        s = s[:-s[-1]]
-        return s
+        return s[:-ord(s[len(s) - 1:])]
