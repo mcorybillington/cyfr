b51205037c3f3456e910f4fb6b18d988ab985bfb
mcorybillington <mcorybillington@gmail.com>
2018-07-30 22:14:02 -0400

    modified to work with new crypto key library

--- crypto/encrypt.py
+++ crypto/encrypt.py
@@ -6,6 +6,7 @@ from Crypto.Cipher import AES
 class EncryptAES(object):
 
     def __init__(self):
+        self.bs = 16
         self.plaintext = ''
         self.private_key = ''
 
@@ -15,10 +16,9 @@ class EncryptAES(object):
         cipher = AES.new(self.private_key, AES.MODE_CBC, iv)
         return base64.b64encode(iv + cipher.encrypt(raw))
 
-    @staticmethod
-    def _pad(s):
+    def _pad(self, s):
         s = str.encode(s)
-        length = 16 - (len(s) % 16)
+        length = self.bs - (len(s) % self.bs)
         s += bytes([length]) * length
         return s
 
