4b2174f73db801bf9cb77d22058fe82fd48e6663
mcorybillington <mcorybillington@gmail.com>
2018-07-31 22:46:16 -0400

    modified to work with X25519

--- crypto/keyexchange.py
+++ crypto/keyexchange.py
@@ -1,22 +1,23 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
+from crypto.mastersecret import MasterSecret
 
 
 class KeyExchange(object):
-    def __init__(self):
-        self.private_key = ''
-        self.peer_public_key = ''
-        self.shared_key = ''
+    def __init__(self, p_key, r_pub_key):
+        self.private_key = p_key
+        self.recv_public_key = r_pub_key
+        self.shared_key = self.get_shared_key()
 
     def get_shared_key(self):
-        return self.private_key.exchange(self.peer_public_key)
+        return self.private_key.exchange(self.recv_public_key)
 
     def get_derived_key(self):
         return HKDF(
-            algorithm=hashes.SHA256(),
-            length=128,
-            salt=None,
-            info=b'handshake data',
-            backend=default_backend()
-        ).derive(self.shared_key)
+                algorithm=hashes.SHA256(),
+                length=32,
+                salt=None,
+                info=b'handshake data',
+                backend=default_backend()
+            ).derive(self.shared_key)
